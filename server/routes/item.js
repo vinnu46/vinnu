e210855b0c3617af4f91c62b39231abc96cac647
MohamedElsayed-22 <103115441+MohamedElsayed-22@users.noreply.github.com>
2023-01-10 05:33:27 +0200

    deployment-commit
    Co-Authored-By: Tameem Alaa El-Deen Sayed Bakr <tameem.alaa@gmail.com>

--- server/routes/item.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const item = require('../controllers/item');
-
-const router = express.Router();
-
-router.get('/',item.getItems);
-router.get('/:itemID',item.getItem);
-
-module.exports = router;
--- server/routes/promotion-code.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const promotionCode = require('../controllers/promotion-code');
-const authMiddleware = require('../controllers/auth-middleware');
-
-const router = express.Router();
-
-router.post('/',authMiddleware,promotionCode.postCheckPromotionCode);
-
-module.exports = router;
